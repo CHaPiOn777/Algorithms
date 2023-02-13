53a4e905bb1cbbcfeacd2a01aca78da93c453510
CHaPiOn777 <89907581+CHaPiOn777@users.noreply.github.com>
2023-02-13 21:20:01 +0300

    начал решать задачку

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,56 @@
-import React from "react";
+import React, { FormEvent, SetStateAction, useEffect, useMemo, useRef, useState } from "react";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
+import { Input } from "../ui/input/input";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import styles from "./string.module.css";
+
 
 export const StringComponent: React.FC = () => {
+  const [inputValue, setInputValue] = useState<string>('');
+  const [isShown, setShown] = useState<boolean>(false);
+  const refq = useRef<HTMLInputElement>(null);
+
+  const onClick = () => {
+    const a = refq;
+    console.log(a)
+    setShown(true)
+  };
+
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    setInputValue(e.currentTarget.value)
+  }
+
+  const letters = inputValue.split('');
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      letters.sort();
+      setInputValue(letters.join(''))
+      console.log(letters)
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [])
+
+
   return (
-    <SolutionLayout title="Строка">
-     
-    </SolutionLayout>
+    <>
+      <SolutionLayout title="Строка">
+        <div className={styles.input}>
+          <Input maxLength={11} ref={refq} onChange={onChange} type="text" />
+          <Button text={'Развернуть'} onClick={onClick} />
+        </div>
+        {isShown &&
+          <ul className={styles.circle}>
+            {letters.map((letter, index) => {
+              return (
+                <li>< Circle letter={letter} key={index}/></li>
+              )
+            })}
+          </ul>
+        }
+      </SolutionLayout>
+    </>
   );
 };
--- src/components/ui/input/input.tsx
+++ src/components/ui/input/input.tsx
@@ -14,6 +14,7 @@ export const Input: React.FC<InputProps> = ({
   maxLength,
   max,
   isLimitText = false,
+
   ...rest
 }) => {
   const limitText =
