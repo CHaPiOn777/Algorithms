feaafcf2e32fa98191513476f8ef10fa041e0458
CHaPiOn777 <89907581+CHaPiOn777@users.noreply.github.com>
2023-02-17 20:41:57 +0300

    доделал перевертываание строки

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,6 +1,4 @@
-import React, { FormEvent, SetStateAction, useEffect, useMemo, useRef, useState } from "react";
-import { clearInterval } from "timers";
-import { ElementStates } from "../../types/element-states";
+import React, { useEffect, useState } from "react";
 import { Button } from "../ui/button/button";
 import { Circle } from "../ui/circle/circle";
 import { Input } from "../ui/input/input";
@@ -10,17 +8,16 @@ import styles from "./string.module.css";
 type TWord = Array<{
   letter: string;
   color: string;
+  id: number;
 }>
 
 export const StringComponent: React.FC = () => {
   const [inputValue, setInputValue] = useState<string>('');
-  const [isShown, setShown] = useState<boolean>(false);
   const [isShownTimeout, setShownTimeout] = useState<boolean>(false);
   const [letters, setLetters] = useState<TWord>();
 
 
   const onClick = () => {
-
     setShownTimeout(true);
   };
 
@@ -29,57 +26,52 @@ export const StringComponent: React.FC = () => {
   }
 
   useEffect(() => {
-    let interval: NodeJS.Timeout | null = null;
     if (isShownTimeout) {
-      const word = Array.from(inputValue).map((letter) => {
-        return { letter: letter, color: 'default' }
+      const word = Array.from(inputValue).map((letter, index) => {
+        return { letter: letter, color: 'default', id: index }
       });
       setLetters(word);
       let start = 0;
       let end = inputValue.length - 1;
-      
-      interval = setInterval(() => {
 
-        if (start < end) {
+      setInterval(() => {
+        if (start <= end) {
           setLetters((oldLetters) => {
             let i = start;
             let j = end;
             const newLetters = [...oldLetters!];
-
-            
             swap(newLetters, start, end);
+
             return newLetters
           })
           start++;
           end--;
-
         } else {
           setShownTimeout(false);
-
         }
       }, 1000)
     }
 
+
   }, [isShownTimeout])
 
-
-  const swap = (arr: TWord, firstIndex: number, secondIndex: number): void => {
-    console.log(secondIndex, firstIndex)
-    if (firstIndex > 0) {
-      arr[secondIndex].color = arr[firstIndex].color = 'changing'
-      arr[secondIndex +1].color = arr[firstIndex - 1].color = 'modified'
-      if (secondIndex - firstIndex === 1) {
-        arr[secondIndex].color = arr[firstIndex].color = 'changing'
-        arr[secondIndex].color = arr[firstIndex].color = 'modified'
-      }
-    } else {
-      arr[secondIndex].color = arr[firstIndex].color = 'changing'
+  const colorSwap = (arr: TWord, firstIndex: number, secondIndex: number): void => {
+    arr[secondIndex].color = arr[firstIndex].color = 'changing';
+    if (secondIndex === firstIndex) {
+      arr[secondIndex].color = 'modified';
     }
+    if (secondIndex - firstIndex < 2) {
+      arr[secondIndex].color = arr[firstIndex].color = 'modified';
+    }
+    if (firstIndex !== 0) {
+      arr[secondIndex + 1].color = arr[firstIndex - 1].color = 'modified';
+    }
+  }
+  const swap = (arr: TWord, firstIndex: number, secondIndex: number): void => {
     const temp = arr[firstIndex];
     arr[firstIndex] = arr[secondIndex];
     arr[secondIndex] = temp;
-   
-    
+    colorSwap(arr, firstIndex, secondIndex)
   }
 
   return (
@@ -92,11 +84,9 @@ export const StringComponent: React.FC = () => {
         {letters &&
           <ul className={styles.circle}>
 
-            {letters?.map(({ letter, color }, index) => {
-
+            {letters?.map(({ letter, color, id }, index) => {
               return (
-
-                <li>< Circle state={color} letter={letter} key={index} /></li>
+                <li>< Circle state={color} letter={letter} key={id} /></li>
               )
             })}
           </ul>
