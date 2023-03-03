c8983d38c3296ccec08c120967663526a46b53d8
CHaPiOn777 <89907581+CHaPiOn777@users.noreply.github.com>
2023-03-03 21:11:33 +0300

    доделал связный список, осталась логика по кнопкам

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -65,15 +65,68 @@ export class LinkedList<T> implements ILinkedList<T> {
       current = this.head;
       while (current.next) {
         current = current.next;
-
       }
       current.next = node;
     }
-    // console.log(this.size)
     this.size++;
     this.tailIndex++;
 
     return this;
+  }
+  addToIndex(element: T, index: number) {
+    const node = new Node(element);
+    let current;
+    let prev;
+    current = this.head;
+    prev = current;
+    if (!this.head) {
+      this.head = node;
+      this.tailIndex++;
+      this.size++
+      return this
+    }
+
+    if (index === 0) {
+      this.head = node;
+      this.head!.next = current;
+      this.tailIndex++;
+      this.size++
+      return this
+    }
+
+    for (let i = 0; i < index; i++) {
+      prev = current;
+      current = current!.next;
+    }
+
+    prev!.next = node;
+    node.next = current;
+    this.tailIndex++;
+    this.size++
+
+    return this
+  }
+
+  popToIndex(index: number) {
+    let current = this.head;
+    let prev = current;
+    if (!this.head) {
+      return this
+    }
+    if (index === 0) {
+      this.head = this.head!.next;
+      this.tailIndex--;
+      this.size--;
+      return this
+    }
+    for (let i = 0; i < index - 1; i++) {
+      prev = current;
+      current = current!.next;
+    }
+    prev!.next = current!.next;
+    this.tailIndex--;
+    this.size--;
+    return this
   }
 
   popHead() {
@@ -91,7 +144,7 @@ export class LinkedList<T> implements ILinkedList<T> {
       return this
     } else {
       let prev = this.head;
-      while(prev.next?.next) {
+      while (prev.next?.next) {
         prev = prev.next
       }
       prev.next = null
@@ -108,7 +161,6 @@ export class LinkedList<T> implements ILinkedList<T> {
       res.push(`${curr.value}`);
       curr = curr.next;
     }
-
     return res;
   }
   getSize = () => this.size;
@@ -125,14 +177,20 @@ export const ListPage: React.FC = () => {
   const [list] = useState(new LinkedList<string>(firstArr));
   const [listItems, setListItems] = useState<string[]>(firstArr);
   const [inputValue, setInputValue] = useState<string>('');
+  const [inputIndex, setInputIndex] = useState<number>(0);
   const [headSmallCircle, setHeadSmallCircle] = useState<number | null>(null);
   const [tailSmallCircle, setTailSmallCircle] = useState<number | null>(null);
   const [headIndex, setHeadIndex] = useState<number>(list.getHeadIndex());
   const [tailIndex, setTailIndex] = useState<number>(list.getTailIndex());
-  const [colorIndex, setColorIndex] = useState<number | null>(null)
+  const [colorSmall, setColorSmall] = useState<number | null>(null);
+  const [colorChanging, setColorChanging] = useState<number | null>(null);
+  const [delItem, setDelItem] = useState<number | null>(null);
 
   const onChange = (e: React.FormEvent<HTMLInputElement>) => {
     setInputValue(e.currentTarget.value);
+  }
+  const onChangeIndex = (e: React.FormEvent<HTMLInputElement>) => {
+    setInputIndex(Number(e.currentTarget.value));
   }
 
   const addTail = async () => {
@@ -142,10 +200,10 @@ export const ListPage: React.FC = () => {
     await delay(time);
     setHeadSmallCircle(null);
     setListItems(list.print());
-    setColorIndex(list.getTailIndex());
+    setColorSmall(list.getTailIndex());
     setTailIndex(list.getTailIndex())
     await delay(time);
-    setColorIndex(null);
+    setColorSmall(null);
   }
 
   const addHead = async () => {
@@ -155,10 +213,49 @@ export const ListPage: React.FC = () => {
     setTailIndex(list.getTailIndex());
     setHeadSmallCircle(null);
     setListItems(list.print());
-    setColorIndex(list.getHeadIndex());
+    setColorSmall(list.getHeadIndex());
     await delay(time);
-    setColorIndex(null);
+    setColorSmall(null);
 
+  }
+
+  const addToIndex = async () => {
+    list.addToIndex(inputValue, inputIndex);
+    for (let i = 0; i <= inputIndex; i++) {
+      await delay(time);
+      setColorChanging(i)
+      setHeadSmallCircle(i);
+    }
+
+    await delay(time);
+    setColorChanging(null)
+    setColorSmall(inputIndex);
+    setTailIndex(list.getTailIndex());
+    setHeadSmallCircle(null)
+    setListItems(list.print());
+    await delay(time);
+
+    setColorSmall(null);
+    await delay(time);
+
+  }
+
+  const popToIndex = async () => {
+    list.popToIndex(inputIndex);
+    for (let i = 0; i <= inputIndex; i++) {
+      setColorChanging(i)
+      await delay(time);
+    }
+    setTailSmallCircle(inputIndex - 1);
+    setDelItem(inputIndex - 1);
+    setColorChanging(inputIndex - 1);
+    await delay(time);
+    setColorChanging(null);
+    setTailSmallCircle(null)
+    setColorSmall(null);
+    setListItems(list.print());
+    setTailIndex(list.getTailIndex());
+    setDelItem(null);
   }
 
   const popTail = async () => {
@@ -180,19 +277,47 @@ export const ListPage: React.FC = () => {
     setListItems(list.print());
     setTailIndex(list.getTailIndex());
   }
+
+  const validation = ((inputIndex > list.getSize() + 1) || (inputIndex < 0)) ? true : false;
+
   return (
     <SolutionLayout title="Связный список">
       <form onChange={e => e.preventDefault()} className={styles.form}>
         <Input extraClass={styles.input} maxLength={4} isLimitText={true} placeholder={'Введите значение'} onChange={e => onChange(e)} />
-        <Button extraClass={styles.btn} text={'Добавить в head'} onClick={addHead} />
-        <Button extraClass={styles.btn} text={'Добавить в tail'} onClick={addTail} />
-        <Button extraClass={styles.btn} text={'Удалить из head'} onClick={popHead} />
-        <Button extraClass={styles.btn} text={'Удалить из tail'} onClick={popTail} />
+        <Button
+          extraClass={styles.btn}
+          text={'Добавить в head'}
+          onClick={addHead}
+        />
+        <Button
+          extraClass={styles.btn}
+          text={'Добавить в tail'}
+          onClick={addTail}
+        />
+        <Button
+          extraClass={styles.btn}
+          text={'Удалить из head'}
+          onClick={popHead}
+        />
+        <Button
+          extraClass={styles.btn}
+          text={'Удалить из tail'}
+          onClick={popTail}
+        />
       </form>
       <form onChange={e => e.preventDefault()} className={`${styles.form} ${styles.formIndex}`}>
-        <Input extraClass={styles.input} value={''} placeholder={'Введите индекс'} onChange={e => onChange(e)} />
-        <Button extraClass={styles.btnIndex} text={'Добавить по индексу'} onClick={addTail} />
-        <Button extraClass={styles.btnIndex} text={'Удалить по индексу'} onClick={addTail} />
+        <Input max={list.getSize() + 1} type='number' isLimitText={true} extraClass={styles.input} placeholder={'Введите индекс'} onChange={e => onChangeIndex(e)} />
+        <Button
+          extraClass={styles.btnIndex}
+          text={'Добавить по индексу'}
+          onClick={addToIndex}
+        />
+        <Button
+          extraClass={styles.btnIndex}
+          text={'Удалить по индексу'}
+          onClick={popToIndex}
+          disabled={validation}
+        />
 
       </form>
       {listItems &&
@@ -207,10 +332,10 @@ export const ListPage: React.FC = () => {
                       <Circle state={'changing'} letter={inputValue} isSmall />
                       : headIndex === index ? 'head' : ''
                   }
-                  letter={item}
+                  letter={delItem === index ? '' : item}
                   key={index}
                   index={index}
-                  state={colorIndex! === index ? 'modified' : 'default'}
+                  state={colorChanging && colorChanging > index ? 'changing' : colorSmall! === index ? 'modified' : 'default'}
                   tail={tailSmallCircle! === index ?
                     <Circle state={'changing'} letter={item} isSmall />
                     : tailIndex! === index ? 'tail' : ''}
